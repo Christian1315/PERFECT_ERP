412a3ef7eddbac8809cff56f68ae3bb25caedd1e
Christian <gogochristian009@gmail.com>
2023-08-14 00:13:24 +0100

    Changement de **E-VOTING** en **ERP-FINANFA**

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>E-VOTING</title>
+    <title>ERP FINANFA</title>
     <link rel="shortcut icon" href="e-voting.jpeg" type="image/x-icon">
     <link rel="stylesheet" href="bootstrap.css">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
@@ -22,7 +22,7 @@
             <div class="col-md-12">
                 <nav class="navbar navbar-expand-lg">
                     <div class="container-fluid">
-                        <a class="navbar-brand text-white bg-success btn roundered px-3" href="/"> <strong>E-VOTING</strong></a>
+                        <a class="navbar-brand text-white bg-success btn roundered px-3" href="/"> <strong>ERP FINANFA</strong></a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
@@ -47,7 +47,7 @@
     <div class="container content">
         <div class="row">
             <div class="col-md-12 text-center">
-                <h1 class="title">Soyez les bienvenu sur l'API de E-VOTING</h1>
+                <h1 class="title">Soyez les bienvenu sur l'API de l'ERP FINANFA</h1>
                 <p class="text-dark">Pour commencer,veuillez jetter un oeil sur la documentation pour avoir une vision plus claire de l'implementation des différentes routes!</p>
                 <a href="#" class="btn documentation bg-success">Voir la documentation</a>
             </div>
