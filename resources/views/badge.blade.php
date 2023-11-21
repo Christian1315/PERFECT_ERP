78718281a259aeb532ab7313ab824d535e24ec10
Christian <gogochristian009@gmail.com>
2023-11-21 21:49:09 +0100

    badge revu

--- resources/views/badge.blade.php
+++ resources/views/badge.blade.php
@@ -13,76 +13,6 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <title>BADGE</title>
 
-    <style>
-        * {
-            /* font-family: "Poppins"; */
-        }
-
-        #card {
-            border: #000 solid 2px;
-            border-radius: 10px;
-            background-image: url("bg-croisade1.png");
-            background-position: center;
-            background-size: cover;
-            background-repeat: no-repeat;
-            padding-top: 150px;
-        }
-
-
-        #header {
-            align-items: center;
-            margin-block: 10px;
-        }
-
-        #card-body {
-            padding-block: 70px;
-        }
-
-
-        p.title {
-            font-size: 22px;
-            margin-bottom: 10px !important;
-            font-size: bold;
-            color: #043311;
-            text-transform: uppercase;
-            font-weight: bold;
-            /* margin-top: -150px; */
-        }
-
-        p.theme {
-            font-size: 20px;
-        }
-
-
-        .reference {
-            background-color: #f9cf00;
-            color: #043311 !important;
-            text-align: center;
-            color: #fff;
-            width: 100%;
-        }
-
-        .fullname {
-            font-size: 30px;
-        }
-
-        .ministry {
-            font-size: 25px;
-        }
-
-        .phone {
-            background-color: #000;
-            color: #fff;
-            padding: 10px;
-            border-radius: 5px;
-        }
-
-        #card-body .avatar {
-            width: 100% !important;
-            /* border-radius: 10px; */
-            border: solid 10px #043311;
-        }
-    </style>
 </head>
 
 <body>
@@ -115,7 +45,7 @@
                             <div class="col-md-12 text-center">
                                 <p class="theme">
                                     <em>Thème:</em>
-                                    <strong> <em>La Révolution Spirituelle pour un ministère pluséfficace et éternellement profitable</em> </strong>
+                                    <strong> <em>La Révolution Spirituelle pour un ministère plus éfficace et éternellement profitable</em> </strong>
                                 </p>
                                 <br>
                                 <div class="row">
