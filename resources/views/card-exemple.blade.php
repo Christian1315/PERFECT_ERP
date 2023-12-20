1b922b033feba8a40ad5bc81fa1523dc46c9d645
Christian <gogochristian009@gmail.com>
2023-12-20 19:26:47 +0100

    format card revue 6

--- resources/views/card-exemple.blade.php
+++ resources/views/card-exemple.blade.php
@@ -166,13 +166,13 @@
                         <div class="row info_block" id="consular_info">
                             <div class="col-6">
                                 <ul class="first">
-                                    <li> <strong> Nom /Name</strong> </li>
-                                    <li> <strong> Prénom(s) /Surname</strong> </li>
-                                    <li> <strong> Télephone /Phone</strong> </li>
-                                    <li> <strong> Email /Email</strong> </li>
-                                    <li> <strong> Titre /Title :</strong> </li>
-                                    <li> <strong> Forme Juridique /Status</strong> </li>
-                                    <li> <strong> Secteur d'activité /Area</strong> </li>
+                                    <li style="font-size: 15px;"> <strong> Nom /Name</strong> </li>
+                                    <li style="font-size: 15px;"> <strong> Prénom(s) /Surname</strong> </li>
+                                    <li style="font-size: 15px;"> <strong> Télephone /Phone</strong> </li>
+                                    <li style="font-size: 15px;"> <strong> Email /Email</strong> </li>
+                                    <li style="font-size: 15px;"> <strong> Titre /Title :</strong> </li>
+                                    <li style="font-size: 15px;"> <strong> Forme Juridique /Status</strong> </li>
+                                    <li style="font-size: 15px;"> <strong> Secteur d'activité /Area</strong> </li>
                                     <!-- <li>Fonction /Function</li> -->
                                     <li> <strong> Dénomination</strong> </li>
                                 </ul>
